60b4d4c95169ebebb1c21de1361503603b169236
Ana <ana.eng.dev@gmail.com>
2020-06-02 21:44:40 -0300

    install correct dependencies to run mocha and make the first test

--- test/test.js
+++ test/test.js
@@ -2,3 +2,20 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');  // inicia com letra maiuscula pois estamos importando uma função construtora que é usado para criar uma instancia do Web3
 const web3 = new Web3(ganache.provider()); // criando uma instancia para se conectar ao localhost ganache
+
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive() {
+        return 'vroom';
+    }
+}
+
+describe('Car', () => {
+    it('park should return a string', () => {
+        const car = new Car();
+        assert.equal(car.park(), 'stopped')
+    });
+})
