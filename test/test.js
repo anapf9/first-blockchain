5cd54c80bdb28c1d43c8a721a46b68974c70a665
Ana <ana.eng.dev@gmail.com>
2020-06-08 10:02:05 -0300

    simple async request ganache provider

--- test/test.js
+++ test/test.js
@@ -3,28 +3,19 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');  // inicia com letra maiuscula pois estamos importando uma função construtora que é usado para criar uma instancia do Web3
 const web3 = new Web3(ganache.provider()); // criando uma instancia para se conectar ao localhost ganache
 
-class Car {
-    park() {
-        return 'stopped';
-    }
+let accounts;
 
-    drive() {
-        return 'vroom';
-    }
-}
-// colocamos a variavel para fora da função para que as posteriores possam saber que a instancia foi iniciada antes delas
-let car; // ussamos o let pois vamos mudar o valor da variavel
-
-beforeEach(() => {
-    car = new Car(); // a variavel car uma nova atribuição
+beforeEach(async () => {
+  //  Get a list of all accounts
+  accounts = await web3.eth.getAccounts() // return a promise beacuse is asyncronus
+    .then(fetchAccounts => {
+      console.log(fetchAccounts)
+    })
+  // Use one of those accounts to deploy the contract
 })
 
-describe('Car', () => {
-    it('park should return a string', () => {
-        assert.equal(car.park(), 'stopped')
-    });
-
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom');
-    });
+describe ('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(accounts)
+  })
 })
