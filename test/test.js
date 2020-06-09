741eb16f3b330b2cc3932b20aef9c1bb50af9c4b
Ana <ana.eng.dev@gmail.com>
2020-06-09 10:31:53 -0300

    update web3 version fix

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');  // inicia com letra maiuscula pois estamos importando uma função construtora que é usado para criar uma instancia do Web3
-const web3 = new Web3(ganache.provider()); // criando uma instancia para se conectar ao localhost ganache
+// Updates
+const provider = ganache.provider(); // criando uma instancia para se conectar ao localhost ganache
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile')
 
 let accounts;
@@ -18,6 +21,9 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' })
+
+  // update
+  inbox.setProvider(provider);
 })
 
 describe ('Inbox', () => {
