871c1cb9c9c4114556f0eb8c350ded0fccb3cae1
Ana <ana.eng.dev@gmail.com>
2020-06-11 16:27:35 -0300

    deploy to rinkeby network

--- deploy.js
+++ deploy.js
@@ -3,7 +3,19 @@ const Web3 = require('web3');
 const {interface, bytecode} = require('./compile');
 
 const provider = new HDWalletProvider(
-    'butter pride reunion dismiss wasp pool giggle tide source pitch during hip',
-    'https://rinkeby.infura.io/v3/7789a4708fbc490dba66cb4efa9f4966'
+    'kiwi neutral carbon century romance capable improve useless exhibit hurry coach immune',
+    'https://rinkeby.infura.io/v3/d397aae599c74761bbbf1a3e444ce0c3'
 );
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Atenção ao deploy da conta', accounts[0])
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+    console.log('Contract deployed to', result.options.address)
+}
+ deploy();
